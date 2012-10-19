932570a13a9495b005673918c9317d0f1f398010
Taciano Tres <tacianotres@gmail.com>
2012-10-19 14:59:50 -0300

    Adicionado comando para executar 'apt-get update' antes das instalações.
    Antes de iniciar a instalação/atualização dos pacotes será atualizada a
    base de dados com as informações dos pacotes disponíveis.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,15 @@
 # Recursos relativos aos pacotes do sistema operacional
 class localhost::packages {
+  exec { 'apt-update':
+    command   => 'apt-get -q update',
+    path      => '/usr/bin',
+    tries     => '3',
+    try_sleep => '60',
+    user      => 'root'
+  }
+
   include localhost::packages::misc
   include localhost::packages::dev
   include localhost::packages::fah
+
 }
