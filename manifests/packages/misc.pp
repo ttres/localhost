eb102363056757318a81e7ecef98b1bd01e3928a
Taciano Tres <tacianotres@gmail.com>
2012-10-19 14:57:00 -0300

    Incluido pacote 'libxml2-utils' e melhorada a documentação.
    Adicionado o pacote 'libxml2-utils' com utilitários para tratar arquivos
    XML; adicionada uma linha de comentário para descrever melhor cada
    pacote a ser instalado.

--- manifests/packages/misc.pp
+++ manifests/packages/misc.pp
@@ -1,12 +1,17 @@
 # Recursos referentes a pacotes gerais do sistema operacional
 class localhost::packages::misc {
-  # Misc
+  # Editor de textos
   package { 'vim': ensure => latest }
 
+  # Substituto do 'top'
   package { 'htop': ensure => latest }
 
+  # Mostra informações da máquina no desktop
   package { 'conky': ensure => latest }
 
   package { 'wget': ensure => latest }
+
+  # pacote para tratamento/validação de arquivos XML
+  package { 'libxml2-utils': ensure => latest }
 
 }
