d92bf038497fa3660990e3bb9e9ebf7c8065fbef
Taciano Tres <tacianotres@gmail.com>
2012-10-19 17:03:44 -0300

    Adicionando 'git-flow'.
    Adicionando os pacotes e arquivos de configuração necessários para
    instalar e configurar o 'git-flow'.

--- manifests/packages/dev.pp
+++ manifests/packages/dev.pp
@@ -1,12 +1,30 @@
 # Recursos referentes à ferramentas de desenvolvimento/codificação
 class localhost::packages::dev {
-  # Development
   package { 'cvs': ensure => latest }
+
+  package { 'subversion': ensure => latest }
 
   package { 'git': ensure => latest }
 
-  package { 'git-flow': ensure => latest }
+  # Pacote para permitir autocomplete no Bash
+  package { 'bash-completion': ensure => latest }
 
-  package { 'subversion': ensure => latest }
+  # https://raw.github.com/bobthecow/git-flow-completion/master/git-flow-completion.bash
+  file { '/etc/bash_completion.d/git-flow':
+    ensure => 'file',
+    source => 'puppet:///modules/localhost/etc/bash_completion.d/git-flow-completion.bash',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  # A collection of Git extensions to provide high-level repository operations for Vincent Driessen's branching model.
+  # https://github.com/nvie/gitflow
+  package { 'git-flow':
+    ensure  => latest,
+    require => [
+      Package['bash-completion'],
+      File['/etc/bash_completion.d/git-flow']]
+  }
 
 }
