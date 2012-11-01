08a8bb7ec1ab2af4cb658669bc20bf2289af823f
Taciano Tres <tacianotres@gmail.com>
2012-10-31 23:59:10 -0200

    Ajustando o caminho dos módulos instalados.
    Para que o Puppet localize os módulos instalados no sistema foi
    necessário adicionar o caminho completo.

--- run-apply.sh
+++ run-apply.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Aplica o módulo 'localhost' no ambiente local
-sudo puppet apply --execute "include localhost" --verbose --modulepath=/home/taciano/workspace-geppetto $@
+sudo puppet apply --execute "include localhost" --modulepath=/home/taciano/workspace-geppetto:/usr/share/puppet/modules $@
