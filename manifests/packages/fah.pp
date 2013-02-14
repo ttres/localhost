6a07798473538a6fbc8feeb45d0bc6caf25b4033
Taciano Tres <tacianotres@gmail.com>
2013-02-13 23:26:58 -0200

    Updating FAH client and control applications.

--- manifests/packages/fah.pp
+++ manifests/packages/fah.pp
@@ -22,57 +22,31 @@ class localhost::packages::fah {
     notify  => Service['FAHClient'],
   }
 
-  /*
-   * # Will not ignore nice load, to speed up processing
-   * exec { 'enable-ignore-nice-load':
-   *  command   => 'ignore.nice.load 1',
-   *  cwd       => '/',
-   *  path      => '/home/taciano/bin:/bin:/usr/bin',
-   *  tries     => '3',
-   *  try_sleep => '60',
-   *  user      => 'root',
-   *}
-   */
-
-  #  file { 'ignore-nice-load':
-  #    ensure  => file,
-  #    path    => '/sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load',
-  #    content => '1',
-  #    owner   => 'root',
-  #    group   => 'root',
-  #    mode    => '0644',
-  #  }
-
-  # https://fah-web.stanford.edu/file-releases/public/release/fahclient/debian-testing-64bit/v7.1/fahclient_7.1.52_amd64.deb
+  # https://fah.stanford.edu/file-releases/public/release/fahclient/debian-testing-64bit/v7.2/fahclient_7.2.9_amd64.deb
   package { 'fahclient':
     ensure   => present,
     provider => 'dpkg',
-    source   => 'puppet:///modules/localhost/fah/fahclient_7.1.52_amd64.deb',
+    source   => 'puppet:///modules/localhost/fah/fahclient_7.2.9_amd64.deb',
     require  => [
       Package['libssl0.9.8'],
       User['fahclient']],
-   #   Exec['enable-ignore-nice-load']],
   }
 
   service { 'FAHClient': ensure => running, }
 
-  # https://fah-web.stanford.edu/file-releases/public/release/fahcontrol/debian-testing-64bit/v7.1/fahcontrol_7.1.52-1_all.deb
+  # https://fah.stanford.edu/file-releases/public/release/fahcontrol/debian-testing-64bit/v7.2/fahcontrol_7.2.9-1_all.deb
   package { 'fahcontrol':
     ensure   => present,
     provider => 'dpkg',
-    source   => 'puppet:///modules/localhost/fah/fahcontrol_7.1.52-1_all.deb',
-    require  => Package['python-gnome2'],
+    source   => 'puppet:///modules/localhost/fah/fahcontrol_7.2.9-1_all.deb',
   }
 
-  /*
-   * # Este pacote não funciona no Ubuntu 12.04. O erro é o seguinte:
-   * # FAHViewer: error while loading shared libraries: libGLEW.so.1.7: cannot open shared object file: No such file or directory
-   *
-   * package { 'fahviewer':
-   * ensure   => present,
-   * provider => 'dpkg',
-   * source   =>
-   * 'https://fah-web.stanford.edu/file-releases/public/release/fahviewer/debian-testing-64bit/v7.1/fahviewer_7.1.52_amd64.deb',
-   *}
-   */
+  # https://fah.stanford.edu/file-releases/public/release/fahviewer/debian-testing-64bit/v7.2/fahviewer_7.2.9_amd64.deb
+  package { 'fahviewer':
+    ensure   => present,
+    provider => 'dpkg',
+    source   => 'puppet:///modules/localhost/fah/fahviewer_7.2.9_amd64.deb',
+    require  => Package['fahclient'],
+  }
 }
+
