f3a04c10f36b4ca0e7fa1cbe42803cc4921e2418
Taciano Tres <tacianotres@gmail.com>
2014-01-21 22:37:55 -0200

    Updating graphics driver packages.

--- manifests/packages/contrib/x11.pp
+++ manifests/packages/contrib/x11.pp
@@ -1,8 +1,14 @@
 #
 class localhost::packages::contrib::x11 {
   # Tool of configuring the NVIDIA graphics driver
-  package { 'nvidia-settings': ensure => latest, }
+  package { 'nvidia-settings': ensure => absent, }
 
   # Tool of configuring the NVIDIA graphics driver
-  package { 'nvidia-settings-updates': ensure => latest, }
+  package { 'nvidia-settings-updates': ensure => absent, }
+
+  # Tool of configuring the NVIDIA graphics driver
+  package { 'nvidia-settings-304': ensure => latest, }
+
+  # Tool of configuring the NVIDIA graphics driver
+  package { 'nvidia-settings-304-updates': ensure => latest, }
 }
--- manifests/packages/restricted/misc.pp
+++ manifests/packages/restricted/misc.pp
@@ -1,9 +1,15 @@
 #
 class localhost::packages::restricted::misc {
   # NVIDIA binary Xorg driver, kernel module and VDPAU library
-  package { 'nvidia-current': ensure => latest, }
+  package { 'nvidia-current': ensure => absent, }
 
   # NVIDIA binary Xorg driver, kernel module and VDPAU library
-  package { 'nvidia-current-updates': ensure => latest, }
+  package { 'nvidia-304': ensure => latest, }
+
+  # NVIDIA binary Xorg driver, kernel module and VDPAU library
+  package { 'nvidia-current-updates': ensure => absent, }
+
+  # NVIDIA binary Xorg driver, kernel module and VDPAU library
+  package { 'nvidia-304-updates': ensure => latest, }
 
 }
