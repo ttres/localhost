67e751a57ac6d9671a2520cb7d53c31eac1427a8
Taciano Tres <tacianotres@gmail.com>
2014-06-24 07:29:48 -0300

    Kernel update.

--- manifests/packages/devel.pp
+++ manifests/packages/devel.pp
@@ -46,35 +46,35 @@ class localhost::packages::devel {
   package { 'libgettextpo0': ensure => latest, }
 
   $old_kernel_packages_real = [
-    'linux-headers-3.2.0-53',
     'linux-headers-3.2.0-56',
     'linux-headers-3.2.0-58',
     'linux-headers-3.2.0-59',
+    'linux-headers-3.2.0-60',
   ]
 
   # Header files related to Linux kernel version 3.2.0
   package { $old_kernel_packages_real: ensure => absent, }
   ->
-  package { 'linux-headers-3.2.0-60': ensure => present, }
+  package { 'linux-headers-3.2.0-61': ensure => latest, }
   ->
-  package { 'linux-headers-3.2.0-61': ensure => present, }
+  package { 'linux-headers-3.2.0-64': ensure => present, }
 
   $old_kernel_headers_packages_real = [
-    'linux-headers-3.2.0-53-generic',
     'linux-headers-3.2.0-56-generic',
     'linux-headers-3.2.0-58-generic',
     'linux-headers-3.2.0-59-generic',
+    'linux-headers-3.2.0-60-generic',
   ]
 
   # Linux kernel headers for version 3.2.0 on 64 bit x86 SMP
   package { $old_kernel_headers_packages_real: ensure => absent, }
   ->
-  package { 'linux-headers-3.2.0-60-generic': ensure => present, }
+  package { 'linux-headers-3.2.0-61-generic': ensure => latest, }
   ->
-  package { 'linux-headers-3.2.0-61-generic': ensure => present, }
+  package { 'linux-headers-3.2.0-64-generic': ensure => present, }
 
   # Linux Kernel Headers for development
-  package { 'linux-libc-dev': ensure => '3.2.0-61.92', }
+  package { 'linux-libc-dev': ensure => '3.2.0-64.97', }
 
   # An utility for Directing compilation.
   package { 'make': ensure => latest, }
--- manifests/packages/kernel.pp
+++ manifests/packages/kernel.pp
@@ -4,16 +4,16 @@ class localhost::packages::kernel {
   package { 'dkms': ensure => latest, }
 
   $old_kernel_images_packages_real = [
-    'linux-image-3.2.0-53-generic',
     'linux-image-3.2.0-56-generic',
     'linux-image-3.2.0-58-generic',
     'linux-image-3.2.0-59-generic',
+    'linux-image-3.2.0-60-generic',
   ]
 
   # Linux kernel image for version 3.2.0 on 64 bit x86 SMP
   package { $old_kernel_images_packages_real: ensure => absent, }
   ->
-  package { 'linux-image-3.2.0-60-generic': ensure => present, }
+  package { 'linux-image-3.2.0-61-generic': ensure => latest, }
   ->
-  package { 'linux-image-3.2.0-61-generic': ensure => present, }
+  package { 'linux-image-3.2.0-64-generic': ensure => present, }
 }
